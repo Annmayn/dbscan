31cf25699b66ca98db8d67b45bb5c434ea2d8ec6
Annmayn <ntvirus333@gmail.com>
2019-03-03 17:17:31 +0545

    updated distance function

--- processor.py
+++ processor.py
@@ -4,5 +4,4 @@ import numpy as np
 def findDistance(n, arr, e):
     dist=np.sqrt(np.sum((arr-n)**2, axis=1))
     #return the index of all such points that satisfy the condition val<e
-    #return the array part of tuple
-    return np.where(dist<=e)[0]
+    return np.where(dist<=e)[0]     #return the array part of tuple
