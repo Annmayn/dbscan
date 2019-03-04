b958abe01596ad5d9ca28a8b8a66570e06aaac5a
Annmayn <ntvirus333@gmail.com>
2019-03-04 19:46:58 +0545

    checkpoint for version 1.0

--- dbscan.py
+++ dbscan.py
@@ -2,11 +2,23 @@ import processor
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#import system
 
-arr=np.array([[1,1],[1,2],[1,3],[4,1],[5,1],[0,0],[6,1],[5,5],[5,6],[6,5]])
+#fixed input
+#arr=np.array([[1,1],[1,2],[1,3],[4,1],[5,1],[0,0],[6,1],[5,5],[5,6],[6,5]])
+
+#read input from csv
+#df = pd.read_csv('../clustering_benchmark_1.csv')
+#arr = np.concatenate( (np.array(df['x']).reshape(df['x'].size,1), np.array(df['y']).reshape(df['y'].size,1)), axis=1)
+
+#custom built dataset from sklearn
+from sklearn import datasets
+
+#arr, y = datasets.make_moons(n_samples=500, factor=0.74, noise=0.15)
+arr, y = datasets.make_blobs(n_samples=500)
+#end of custom dataset
+
 e=2
-minPoint=1
+minPoint=3
 
 #arr = system.args[2]
 #generate queue of random selection
@@ -23,11 +35,12 @@ clusterNum = 1
 
 def isCoreNode(i, arr, e, minPoint):
     global clusterNum
-    #define the stack for neighbor searching
+    #define the stack for neighbor searching using dfs
     customQueueArr = []
     ind_lst = processor.findDistance(arr[i], arr, e)
+    print(len(ind_lst))
     #not core node
-    if len(ind_lst)<minPoint and clusterInfo[i]<1:
+    if len(ind_lst)<minPoint and clusterInfo[i]<1: #clusterInfo[i]<1 makes sure classified nodes aren't affected
         clusterInfo[i]=-1
         
     #if core node 
@@ -39,10 +52,10 @@ def isCoreNode(i, arr, e, minPoint):
             #remove from global random sample list
             if ind in randomPointQueue:
                 randomPointQueue.remove(ind)
-            #remove from stack
-            if ind in customQueueArr: customQueueArr.remove(ind)
-            #remove current node
-            if ind != i:    tmpArr+=ind
+                #remove from stack
+                if ind in customQueueArr: customQueueArr.remove(ind)
+                #ignore current node
+                if ind != i:    tmpArr+=ind
         customQueueArr = np.append(tmpArr, customQueueArr).tolist()
         
     while len(customQueueArr)!=0:
@@ -58,10 +71,16 @@ def runDbscan(arr, e, minPoint):
         randomPointQueue = randomPointQueue[1:]  
         isCoreNode(ind, arr, e, minPoint)
 
+#run the main code
+runDbscan(arr, e, minPoint)   
 
-runDbscan(arr, e, minPoint)     
+#display number of clusters found
+print("Number of cluster detected: ",clusterNum-1)  
+
 df = pd.DataFrame()
 df['x'] = arr[:,0]
 df['y'] = arr[:,1]
 df['clusterInfo'] = clusterInfo
-plt.scatter(df['x'], df['y'], c=clusterInfo)
+
+plt.scatter(df['x'][clusterInfo==-1], df['y'][clusterInfo==-1], s=20, marker='*', c='black')
+plt.scatter(df['x'][clusterInfo!=-1], df['y'][clusterInfo!=-1], marker='o', c=clusterInfo[clusterInfo!=-1])
